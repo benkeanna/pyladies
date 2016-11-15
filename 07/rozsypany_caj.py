0b8101a8527f4e3e0888abec8f39414d33998048
Anna Haymannová <haymannovaa@gmail.com>
2016-11-15 15:46:21 +0100

    rozsypany caj

---
+++ 07/rozsypany_caj.py
@@ -0,0 +1,18 @@
+hiragana = "ぁあぃいぅうぇえぉおかがきぎくぐけげこごさざしじすずせぜそぞただちぢっつづてでとどなにぬねのはばぱひびぴふぶぷへべぺほぼぽまみむめもゃやゅゆょよらりるれろゎわゐゑをんゔ"
+hira_count = 0
+
+katakana = "ァアィイゥウェエォオカガキギクグケゲコゴサザシジスズセゼソゾタダチヂッツヅテデトドナニヌネノハバパヒビピフブプヘベペホボポマミムメモャヤュユョヨラリルレロヮワヰヱヲンヴヵヶヷヸヹヺ"
+kata_count = 0
+
+with open("rozsypany_caj.txt") as my_file:
+    for line in my_file:
+        for ch in line:
+            if ch in hiragana:
+                hira_count = hira_count + 1
+            elif ch in katakana:
+                kata_count = kata_count + 1
+            else:
+                pass
+
+    print("Počet znaků z abecedy hiragana je ", hira_count,
+          ", počet znaků z abecedy katakana je ", kata_count, ".")
