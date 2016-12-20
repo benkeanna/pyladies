6280bb6a229d95a2bc1bb2db1c6f72973465e338
Anna Haymannová <haymannovaa@gmail.com>
2016-12-20 15:26:40 +0100

    codecember 8 upravy

--- Codecember/Codecember_08.py
+++ Codecember/Codecember_08.py
@@ -6,6 +6,7 @@
 # Nakonec mu ještě appka vypíše, jak dlouho by musel běžet za sáněmi (místo toho, aby se vezl), aby spálil rovnocenné množství energie.
 # Mikuláš ví, že za hodinu mírného běhu spálí 600 kcal. Například když si Mikuláš dá svůj oblíbený Pribináček, zadá (podle obalu) 16,1 % tuků, 15,8 % sacharidů a 7,4 % bílkovin.
 # Pribináček má hmotnost 80 gramů, tedy asi 190 kcal energie a zhruba 19 minut běhání.
+import math
 
 total_weight = int(input('Enter weight of your food. ')) # vstupní hodnoty váhy jídla a procent jednotlivých komponent
 fat_perc = int(input('Enter percent of fat in your food. '))
@@ -23,21 +24,23 @@ def get_calories(component_weight, kcal): # funkce na výpočet kalorií jednotl
     calories = component_weight * kcal
     return calories
 
-def get_time_of_running(total_calories):
+def get_time_of_running(total_calories): # funkce na výpočet času, jak dlouho musí běhat vzhledem k přijatým kaloriím
     running_time = (60 * total_calories) / 600
     return running_time
 
 food_weight = []
-for item_perc in food_perc:
+for item_perc in food_perc: # plnění nového seznamu váhy komponent vycházející z procentuálního podílu na váze jídla
     food_weight.append(get_weight_of_component(item_perc))
 
 food_calories = []
 i = 0
-for item_weight in food_weight:
+for item_weight in food_weight: # plnění nového seznamu kalorií komponent z jejich váhy
     food_calories.append(get_calories(item_weight,kcal_in_food[i]))
-    i += 1
+    i += 1 # index kvůli použití hodnoty kalorií na komponentu ze seznamu na řádku 17
 
-total_calories = sum(food_calories)
-total_running_time = get_time_of_running(total_calories)
+total_calories = sum(food_calories) # celkové mmožství přijatých kalorií
+total_running_time = get_time_of_running(total_calories) # počet minut, kdy by měl běhat by spálil kalorie
+hour_total_running_time = math.floor(total_running_time / 60) # počet hodin běhání
+minute_total_running_time = total_running_time % 60 # počet zbývajících minut běhání
 
-print('You will have to run', total_running_time, 'minut.')
+print('You will have to run', hour_total_running_time, 'hours and',minute_total_running_time,'minutes.')
