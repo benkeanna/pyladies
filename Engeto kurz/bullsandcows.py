f9e697e259c44b10c96e1ad1918330b82d14c940
Anna Haymannová <haymannovaa@gmail.com>
2017-01-11 18:16:06 +0100

    hra bulls and cows

---
+++ Engeto kurz/bullsandcows.py
@@ -0,0 +1,66 @@
+# game bulls and cows in python 3
+
+import random
+puzzle = random.sample(range(1, 10), 4)
+number_of_guesses = 0 # pocet pokusu, ktery se bude zsvetsovat po kazdem tahu
+
+class CheckInput: # kontrola, jestli je input 4-mistne cislo s ruznymi cislicemi
+    def __init__(self, user_input):
+        self.input = user_input
+
+    def check_if_input_is_int(self): # kontrola, ze vstup jsou cislice
+        try:
+            int(self.input)
+        except ValueError:
+            return False
+        else:
+            return True
+
+    def check_digits_count(self): # kontrola 4-mistneho vstupu
+        return True if len(str(self.input)) == 4 else False
+
+    def check_if_digits_are_different(self): # kontrola, ze input obsahuje 4 ruzne cislice
+        number_of_occur = []
+        for number in self.input:
+            number_of_occur.append(self.input.count(number)) # pocet vyskytu jednotlivych polozek v inputu
+        return True if sum(number_of_occur) == len(str(self.input)) else False # suma poctu ma byt stejna jako delka inputu - kazda polozka je v nem jenom jednou
+
+
+print('Welcome in game cows and bulls. In this game you will have to quess the puzzle made of four different digits.')
+while True:
+    while True: # overeni validity inputu
+        user_input = input('Enter four different digits ')
+        guess_input = CheckInput(user_input)
+        if guess_input.check_digits_count() and guess_input.check_if_input_is_int() and guess_input.check_if_digits_are_different(): # overeni, jestli je vstup 4-mistne cislo s ruznymi cislicemi
+            break
+        else:
+            print('These are not four different digits')
+            pass
+
+    guess = list(guess_input.input) # vytvoreni seznamu z inputu pro porovnani s puzzle
+    guess = [ int(x) for x in guess ] # pretypovani elementu v seznamu na cislice
+
+    list_of_bulls = [i for i, j in zip(puzzle, guess) if i == j] # zjisteni poctu bulls
+    bulls = len(list_of_bulls)
+    print(bulls,'bulls')
+
+    list_of_cows = set(puzzle) & set(guess) # zjiteni poctu cows
+    cows = (len(list_of_cows) - bulls)
+    print(cows,'cows')
+    number_of_guesses += 1
+
+    if bulls == len(puzzle): # vyhodnoceni hry (uzivatel uhadnul)
+        break
+    else:
+        pass
+
+if number_of_guesses < 6: # vyhodnoceni uspesnosti
+    success_rate = 'Awesome'
+elif number_of_guesses >= 6 and number_of_guesses < 10:
+    success_rate = 'Good'
+elif number_of_guesses >= 10 and number_of_guesses < 20:
+    success_rate = 'I quess it is good'
+elif number_of_guesses >= 20:
+    success_rate = 'Not so good'
+
+print('Congratulation, you made it with',number_of_guesses,'guesses.',success_rate,'job.')
