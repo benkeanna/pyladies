061e3382b935040b99a8e497a77b07556f224d55
Anna Haymannová <haymannovaa@gmail.com>
2017-01-12 21:38:59 +0100

    hra bulls and cows

--- Engeto kurz/bullsandcows.py
+++ Engeto kurz/bullsandcows.py
@@ -5,6 +5,7 @@ field_size = 4 # pocet cislic, ktere hrac hada
 puzzle = random.sample(range(1, 10), field_size) # nahodne generovane cislice, ktere hrac hada
 number_of_guesses = 0 # pocet pokusu, ktery se bude zvetsovat po kazdem tahu hrace
 
+
 class CheckInput: # kontrola, jestli je vstup 4-mistne cislo s ruznymi cislicemi
     def __init__(self, user_input):
         self.input = user_input
@@ -27,19 +28,21 @@ class CheckInput: # kontrola, jestli je vstup 4-mistne cislo s ruznymi cislicemi
         return True if sum(number_of_occur) == len(str(self.input)) else False # suma vyskytu jednotlivých cislic ma byt stejna jako delka vstupu - kazda polozka je v nem jenom jednou
 
 
-print('Welcome in game cows and bulls. In this game you will have to quess the puzzle made of four different digits.')
+print('Welcome in game cows and bulls. ')
 while True:
     while True: # vstup a overeni jeho validity
         user_input = input('Enter four different digits ')
         guess_input = CheckInput(user_input)
-        if guess_input.check_digits_count() and guess_input.check_if_input_is_int() and guess_input.check_if_digits_are_different(): # overeni, jestli je vstup 4-mistne cislo s ruznymi cislicemi
+        if (guess_input.check_digits_count() # overeni, jestli je vstup 4-mistne cislo s ruznymi cislicemi
+            and guess_input.check_if_input_is_int()
+            and guess_input.check_if_digits_are_different()):
             break
         else:
             print('These are not four different digits')
             pass
 
     guess = list(guess_input.input) # vytvoreni seznamu z inputu pro porovnani s puzzle
-    guess = [ int(x) for x in guess ] # pretypovani elementu v seznamu na cislice
+    guess = [int(x) for x in guess] # pretypovani elementu v seznamu na cislice
 
     list_of_bulls = [i for i, j in zip(puzzle, guess) if i == j] # zjisteni vsech bulls
     bulls = len(list_of_bulls) # zjisteni pocet bulls
@@ -64,4 +67,5 @@ elif number_of_guesses >= 10 and number_of_guesses < 20:
 elif number_of_guesses >= 20:
     success_rate = 'Not so good'
 
-print('Congratulation, you made it with',number_of_guesses,'guesses.',success_rate,'job.')
+print('Congratulation, you made it with',number_of_guesses,'guesses.',end=' ')
+print(success_rate,'job.')
